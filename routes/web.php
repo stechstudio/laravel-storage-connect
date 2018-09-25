61f07d637fac2d3a06ed6b5a67b80bc2feb9069d
Joseph <joseph@stechstudio.com>
2018-09-25 17:10:28 -0400

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 Route::prefix(config('storage-connect.path'))->middleware(config('storage-connect.middleware'))->group(function() {
-
     if(config('storage-connect.authorize_route')) {
         Route::get('authorize/{driver?}', function ($driver) {
             return Auth::user()->getCloudStorage($driver)->authorize(request('redirect'));
