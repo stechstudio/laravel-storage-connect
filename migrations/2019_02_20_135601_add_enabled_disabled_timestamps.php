ac6e5c7fec3c1b97edca96e8d56cfbd0c8d543c8
Joseph <joseph@stechstudio.com>
2019-02-20 14:05:53 -0500

    populate the timestamps

--- migrations/2019_02_20_135601_add_enabled_disabled_timestamps.php
+++ migrations/2019_02_20_135601_add_enabled_disabled_timestamps.php
@@ -15,6 +15,16 @@ class AddEnabledDisabledTimestamps extends Migration
             $table->timestamp('enabled_at')->nullable();
             $table->timestamp('disabled_at')->nullable();
         });
+
+        // Need to make sure we have some enabled_at date for all currently enabled connections
+        \STS\StorageConnect\Models\CloudStorage::where('enabled', 1)->get()->each(function($storage) {
+            $storage->update(['enabled_at' => $storage->created_at]);
+        });
+
+        // Same for disabled
+        \STS\StorageConnect\Models\CloudStorage::where('enabled', 0)->get()->each(function($storage) {
+            $storage->update(['disabled_at' => $storage->updated_at]);
+        });
     }
 
     /**
